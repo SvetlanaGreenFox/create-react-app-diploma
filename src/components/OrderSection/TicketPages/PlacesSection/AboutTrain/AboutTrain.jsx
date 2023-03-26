d3cbc505385a620eaf2be0fb9426dcbaaff2eb8f
Svetlana <svetlanagreenfox@gmail.com>
2023-03-26 18:36:58 +0300

    Started stylizing Places section

---
+++ src/components/OrderSection/TicketPages/PlacesSection/AboutTrain/AboutTrain.jsx
@@ -0,0 +1,9 @@
+import styles from './AboutTrain.module.scss';
+
+const AboutTrain = () => {
+    return (
+        <section className={styles['about-train']}>ghbghgjb</section>
+    )
+};
+
+export default AboutTrain;
--- src/components/OrderSection/TicketPages/PlacesSection/PlacesSection.jsx
+++ src/components/OrderSection/TicketPages/PlacesSection/PlacesSection.jsx
@@ -1,10 +1,14 @@
 import styles from './PlacesSection.module.scss';
 import { NavLink } from 'react-router-dom';
 
+import AboutTrain from './AboutTrain/AboutTrain';
+
 
 const PlacesSection = () => {
     return (
         <section className={styles.places}>
+            <h3 className={styles['places__title']}>Выбор мест</h3>
+            <AboutTrain />
             <button onClick={() => console.log('Work2')}>
                 <NavLink to='/order/list/tickets'>Вернуться к выбору билетов</NavLink>
             </button>
