77d84369889748152ab378e5d14c441af0a41103
Svetlana <svetlanagreenfox@gmail.com>
2023-04-17 15:39:18 +0300

    Clear PriceHandler

--- src/components/OrderSection/TicketPages/AsideSection/SortMenu/PriceHandler/PriceHandler.jsx
+++ src/components/OrderSection/TicketPages/AsideSection/SortMenu/PriceHandler/PriceHandler.jsx
@@ -2,7 +2,6 @@ import styles from './PriceHandler.module.scss';
 import MultiRangeSlider from './MultiRangeSlider';
 
 import { useState } from 'react';
-// import MultiRangeSlider from "multi-range-slider-react";
 
 const PriceHandler = () => {
   return(
@@ -10,7 +9,6 @@ const PriceHandler = () => {
       <MultiRangeSlider
         min={1920}
         max={7000}
-        //  onChange={({ min, max }) => console.log(`min = ${min}, max = ${max}`)}
       />
     </div>
   )
