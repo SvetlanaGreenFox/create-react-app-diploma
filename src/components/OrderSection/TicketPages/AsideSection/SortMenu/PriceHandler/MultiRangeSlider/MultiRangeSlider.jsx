67ac9633c47a8f5b131b9db3f36c19d0c11660c4
Svetlana <svetlanagreenfox@gmail.com>
2023-04-17 15:37:12 +0300

    Added price slider

--- src/components/OrderSection/TicketPages/AsideSection/SortMenu/PriceHandler/MultiRangeSlider/MultiRangeSlider.jsx
+++ src/components/OrderSection/TicketPages/AsideSection/SortMenu/PriceHandler/MultiRangeSlider/MultiRangeSlider.jsx
@@ -12,6 +12,9 @@ const MultiRangeSlider = ({ min, max }) => {
 
     const minValRef = useRef(null);
     const maxValRef = useRef(null);
+    const parentRef = useRef(null);
+    const maxRef = useRef(null);
+
     const range = useRef(null);
     
     // Convert to percentage
@@ -24,67 +27,83 @@ const MultiRangeSlider = ({ min, max }) => {
     if (maxValRef.current) {
         const minPercent = getPercent(minVal);
         const maxPercent = getPercent(+maxValRef.current.value); 
-        console.log('min', minPercent);
-        console.log('max',maxPercent);
 
         if (range.current) {
-        range.current.style.left = `${minPercent}%`;
-        range.current.style.width = `${maxPercent - minPercent}%`;
+            range.current.style.left = `${minPercent}%`;
+            range.current.style.width = `${maxPercent - minPercent}%`;
         }
     }
     }, [minVal, getPercent]);
 
     // Set width of the range to decrease from the right side
     useEffect(() => {
+        
     if (minValRef.current) {
         const minPercent = getPercent(+minValRef.current.value);
         const maxPercent = getPercent(maxVal);
 
-        if (range.current) {
+        if (range.current) { 
         range.current.style.width = `${maxPercent - minPercent}%`;
+        let valueMaxRef = `${maxPercent - minPercent - 5}%`; 
+        
+        if (maxPercent - minPercent > 20 && maxPercent - minPercent < 80){
+            valueMaxRef = valueMaxRef;
+        }
+        if (maxPercent - minPercent >= 80) valueMaxRef = `75%`;
+        if (maxPercent - minPercent < 20) valueMaxRef = `15%`;
+            maxRef.current.style.left = valueMaxRef;
         }
     }
     }, [maxVal, getPercent]);
 
     // Get min and max values when their state changes
-    // useEffect(() => {
-    //     onChange({ min: minVal, max: maxVal });
-    // }, [minVal, maxVal, onChange]);
+    useEffect(() => {
+        setMaxVal(min * 2);
+    }, [min, max]);
 
     return (
-        <div className={styles.container}>
-            <input
-                type="range"
-                min={ min }
-                max={ max }
-                ref={minValRef}
-                onChange={(event) => {
-                    const value = Math.min(+event.target.value, maxVal - 1);
-                    setMinVal(value);
-                    event.target.value = value.toString();
-                  }}
-                className={cn(styles['thumb'], styles['thumb--zindex-3'], {
-                    "thumb--zindex-5": minVal > max - 100
-                })}
-            />
-            <input
-                type="range"
-                min={ min }
-                max={ max }
-                ref={ maxValRef }
-                onChange={(event) => {
-                    const value = Math.max(+event.target.value, minVal + 1);
-                    setMaxVal(value);
-                    event.target.value = value.toString();
-                  }}
-                className={cn(styles['thumb'], styles['thumb--zindex-4'])}
-            />
-            <div className={styles['slider']}>
-                <div className={styles['slider__track']} />
-                <div className={styles['slider__range']} />
-                <div ref={range} className={styles['slider__range']} />
-                <div className={styles['slider__left-value']}>{minVal}</div>
-                <div className={styles['slider__right-value']}>{maxVal}</div>
+        <div >
+            <div className={styles.header}>
+                <p>от</p>
+                <p>до</p>
+            </div>
+            <div className={styles.container}>
+                <input
+                    type="range"
+                    min={ min }
+                    max={ max }
+                    value={minVal}
+                    ref={minValRef}
+                    onChange={(event) => {
+                        const value = Math.min(+event.target.value, maxVal - 1);
+                        setMinVal(value);
+                        event.target.value = value.toString();
+                    }}
+                    className={cn(styles['thumb'], styles['thumb--zindex-3'], {
+                        "thumb--zindex-5": minVal > max - 100
+                    })}
+                />
+                <input
+                    type="range"
+                    min={ min }
+                    max={ max }
+                    value={maxVal}
+                    ref={ maxValRef }
+                    onChange={(event) => {
+                        const value = Math.max(+event.target.value, minVal + 1);
+                        setMaxVal(value);
+                        event.target.value = value.toString();
+                    }}
+                    className={cn(styles['thumb'], styles['thumb--zindex-4'])}
+                />
+                <div ref={ parentRef } className={styles['slider']}>
+                    <div className={styles['slider__track']} />
+                    <div className={styles['slider__range']} />
+                    <div ref={ range } className={styles['slider__range']} />
+                    <div className={styles['slider__left-value']}>{minVal}</div>
+                    <div ref={ maxRef } className={styles['slider__right-value']}>{maxVal}</div>
+                    <div className={styles['slider__max-value']}>{max}</div>
+                </div>
             </div>
         </div>
     )
