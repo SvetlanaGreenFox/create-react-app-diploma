6e800c3d85e6000f7d6753a23e5e5cd92e8da1fb
Svetlana <svetlanagreenfox@gmail.com>
2023-04-18 16:11:32 +0300

    Added time slider

--- src/components/OrderSection/TicketPages/AsideSection/SortMenu/TimeHandler/TimeHandler.jsx
+++ src/components/OrderSection/TicketPages/AsideSection/SortMenu/TimeHandler/TimeHandler.jsx
@@ -1,10 +1,19 @@
 import styles from './TimeHandler.module.scss';
 
+import TimeRangeSlider from './TimeRangeSlider';
+// import MultiRangeSlider from '../PriceHandler/MultiRangeSlider';
+
+
 const TimeHandler = () => {
 
     return (
         <div className={styles['wrapper']}>
-            <div className={styles['time-wrapper__item']}>ghbghgbj</div>
+            <div className={styles['time-wrapper__item']}>
+                <TimeRangeSlider
+                    max={1439}
+                    min={0}
+                />
+            </div>
             <div className={styles['time-wrapper__item']}>fbffb</div>
         </div>
     )
--- src/components/OrderSection/TicketPages/AsideSection/SortMenu/TimeHandler/TimeRangeSlider.jsx
+++ src/components/OrderSection/TicketPages/AsideSection/SortMenu/TimeHandler/TimeRangeSlider.jsx
@@ -1,14 +1,14 @@
-import styles from './MultiRangeSlider.module.scss';
+import styles from './TimeRangeSlider.module.scss';
 
 import { useState, useRef, useCallback, useEffect } from 'react';
 
 import cn from 'classnames';
 
-
-
-const MultiRangeSlider = ({ min, max }) => {
+const TimeRangeSlider = ({ min, max }) => {
     const [minVal, setMinVal] = useState(min);
     const [maxVal, setMaxVal] = useState(max);
+    const [minTime, setMinTime] = useState();
+    const [maxTime, setMaxTime] = useState();
 
     const minValRef = useRef(null);
     const maxValRef = useRef(null);
@@ -22,13 +22,27 @@ const MultiRangeSlider = ({ min, max }) => {
         (value) => Math.round(((value - min) / (max - min)) * 100), [min, max]
     );
 
-    // Set width of the range to decrease from the left side
+
+    const minuteToTime = (value) => {
+        value = (value > 1439) ? 1439 : value;
+        let hours = Math.floor(value / 60),
+        minutes = value - (hours * 60);
+        
+        if (`${hours}`.length === 1) hours = '0' + hours;
+        if (`${minutes}`.length === 1) minutes = '0' + minutes;
+        if (minutes === 0) minutes = '00';
+            
+        return {hours: hours, minutes: minutes};
+    }
+    
     useEffect(() => {
     if (maxValRef.current) {
         const minPercent = getPercent(minVal);
         const maxPercent = getPercent(+maxValRef.current.value); 
-
+        
         if (range.current) {
+            const prepareTime = `${minuteToTime(minVal).hours}:${minuteToTime(minVal).minutes}`;
+            setMinTime(prepareTime);
             range.current.style.left = `${minPercent}%`;
             range.current.style.width = `${maxPercent - minPercent}%`;
         }
@@ -38,31 +52,34 @@ const MultiRangeSlider = ({ min, max }) => {
     // Set width of the range to decrease from the right side
     useEffect(() => {
         
-    if (minValRef.current) {
-        const minPercent = getPercent(+minValRef.current.value);
-        const maxPercent = getPercent(maxVal);
+        if (minValRef.current) {
+            const minPercent = getPercent(+minValRef.current.value);
+            const maxPercent = getPercent(maxVal);
 
-        if (range.current) { 
-        range.current.style.width = `${maxPercent - minPercent}%`;
-        let valueMaxRef = `${maxPercent - minPercent - 5}%`; 
-        
-        if (maxPercent - minPercent >= 80) valueMaxRef = `75%`;
-        if (maxPercent - minPercent < 20) valueMaxRef = `15%`;
-            maxRef.current.style.left = valueMaxRef;
+            if (range.current) { 
+            range.current.style.width = `${maxPercent - minPercent}%`;
+            let valueMaxRef = `${maxPercent - minPercent - 5}%`;
+            
+            const prepareTime = `${minuteToTime(maxVal).hours}:${minuteToTime(maxVal).minutes}`;
+            setMaxTime(prepareTime);
+            
+            // if (maxPercent - minPercent > 20 && maxPercent - minPercent < 75) valueMaxRef;
+            if (maxPercent - minPercent >= 75) valueMaxRef = `70%`;
+            if (maxPercent - minPercent < 20) valueMaxRef = `15%`;
+                maxRef.current.style.left = valueMaxRef;
+            }
         }
-    }
     }, [maxVal, getPercent]);
 
     // Get min and max values when their state changes
     useEffect(() => {
-        setMaxVal(min * 2);
+        // setMaxVal(min * 2);
     }, [min, max]);
 
     return (
         <div >
             <div className={styles.header}>
-                <p>от</p>
-                <p>до</p>
+                <p>Время отбытия</p>
             </div>
             <div className={styles.container}>
                 <input
@@ -76,15 +93,13 @@ const MultiRangeSlider = ({ min, max }) => {
                         setMinVal(value);
                         event.target.value = value.toString();
                     }}
-                    className={cn(styles['thumb'], styles['thumb--zindex-3'], {
-                        "thumb--zindex-5": minVal > max - 100
-                    })}
+                    className={cn(styles['thumb'], styles['thumb--zindex-3'])}
                 />
                 <input
                     type="range"
                     min={ min }
                     max={ max }
-                    value={maxVal}
+                    value={ maxVal }
                     ref={ maxValRef }
                     onChange={(event) => {
                         const value = Math.max(+event.target.value, minVal + 1);
@@ -97,13 +112,13 @@ const MultiRangeSlider = ({ min, max }) => {
                     <div className={styles['slider__track']} />
                     <div className={styles['slider__range']} />
                     <div ref={ range } className={styles['slider__range']} />
-                    <div className={styles['slider__left-value']}>{minVal}</div>
-                    <div ref={ maxRef } className={styles['slider__right-value']}>{maxVal}</div>
-                    <div className={styles['slider__max-value']}>{max}</div>
+                    <div className={styles['slider__left-value']}>{minTime}</div>
+                    <div ref={ maxRef } className={styles['slider__right-value']}>{maxTime}</div>
+                    <div className={styles['slider__max-value']}>24:00</div>
                 </div>
             </div>
         </div>
     )
 };
 
-export { MultiRangeSlider };
+export default TimeRangeSlider;
